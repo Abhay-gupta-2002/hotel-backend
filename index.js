6d443c8d902958e75470339d37eb2c36c38f9fe0
Abhay-gupta-2002 <aashutoshgupta3323@gmail.com>
2025-10-03 19:34:29 +0530

    shift mongodb to online server mongodb_atlas

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const app = express();
 const db=require('./db');
+require('dotenv').config();
+const PORT=process.env.PORT||3000;
 const Person=require('./models/Person');
 const MenuItem=require('./models/MenuItem');
 const bodyParser=require('body-parser');
@@ -12,4 +14,5 @@ const menuRoutes=require('./routes/menuRoutes');
 const personRoutes=require('./routes/personRoutes');
 app.use('/menu',menuRoutes);
 app.use('/person',personRoutes);
-app.listen(3000,()=>{console.log("server is listening")})
+
+app.listen(PORT,()=>{console.log("server is listening")})

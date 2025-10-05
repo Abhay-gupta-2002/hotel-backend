71ac72f0cedcef4cf517a30b4469f5dea9754b5a
Abhay-gupta-2002 <aashutoshgupta3323@gmail.com>
2025-10-05 20:25:39 +0530

    2nd commit

---
+++ auth.js
@@ -0,0 +1,28 @@
+const passport = require("passport");
+const LocalStrategy = require("passport-local").Strategy;
+const Person = require("./models/Person");
+passport.use(
+  new LocalStrategy(
+    { usernameField: 'username', passwordField: 'password' },
+    async (username, password, done) => {
+      try {
+        const user = await Person.findOne({ username });
+        if (!user) {
+          return done(null, false, { message: 'User not found' });
+        }
+
+        const isMatch =  user.comparePassword(password);
+        if (isMatch) {
+          return done(null, user);
+        } else {
+          return done(null, false, { message: 'Incorrect password' });
+        }
+
+      } catch (err) {
+        console.error("Error in authentication:", err);
+        return done(err);
+      }
+    }
+  )
+);
+module.exports = passport;
--- index.js
+++ index.js
@@ -1,18 +1,30 @@
 const express = require('express')
+
 const app = express();
 const db=require('./db');
 require('dotenv').config();
-const PORT=process.env.PORT||3000;
-const Person=require('./models/Person');
-const MenuItem=require('./models/MenuItem');
+const passport=require('./auth');
+
+// const Person=require('./models/Person');
+// const MenuItem=require('./models/MenuItem');
 const bodyParser=require('body-parser');
 app.use(bodyParser.json());
-app.get('/', (req, res) => {
-  res.send('this is list of menu')
+const PORT=process.env.PORT||3000;
+const logRequest=(req,res,next)=>{
+  console.log(`${new Date().toLocaleString()} Request made to :${req.originalUrl}`);
+  next();
+}
+app.use(logRequest);
+
+app.use(passport.initialize());
+const localAuthMiddleware=passport.authenticate('local',{session:false});
+app.get('/',localAuthMiddleware,function (req, res){
+  res.send('this is list of menu');
 })
 const menuRoutes=require('./routes/menuRoutes');
 const personRoutes=require('./routes/personRoutes');
-app.use('/menu',menuRoutes);
-app.use('/person',personRoutes);
+const Person = require('./models/Person');
+app.use('/menu',localAuthMiddleware,menuRoutes);
+app.use('/person',localAuthMiddleware,personRoutes);
 
 app.listen(PORT,()=>{console.log("server is listening")})
